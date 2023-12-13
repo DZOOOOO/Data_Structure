c947a37281f54a0c5b2ee8c710fbb34cb15b963a
DZOOOOO <dzpro0327@naver.com>
2023-12-13 18:11:32 +0900

    gong add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // 합의 법칙 -> 사건A 또는 사건B 가 일어날 경우의 수
+        System.out.println("== 합의 법칙 ==");
+
+        int[] dice1 = {1, 2, 3, 4, 5, 6};
+        int[] dice2 = {1, 2, 3, 4, 5, 6};
+
+        int nA = 0;
+        int nB = 0;
+        int nAandB = 0;
+
+        // 두 개의 주사위를 던졌을 때 합이 3 또는 4의 배수인 경우의 수
+        for (int i : dice1) {
+            for (int j : dice2) {
+                if ((i + j) % 3 == 0) {
+                    nA += 1;
+                } else if ((i + j) % 4 == 0) {
+                    nB += 1;
+                } else if ((i + j) % 12 == 0) {
+                    nAandB += 1;
+                }
+            }
+        }
+
+        System.out.println(nA + nB - nAandB);
+
+        // HashSet 이용
+        HashSet<ArrayList> allCase = new HashSet<>();
+        for (int item1 : dice1) {
+            for (int item2 : dice2) {
+                if ((item1 + item2) % 3 == 0 || (item1 + item2) % 4 == 0) {
+                    ArrayList list = new ArrayList(Arrays.asList(item1, item2));
+                    allCase.add(list);
+                }
+            }
+        }
+        System.out.println(allCase.size());
+
+
+        // 곱의 법칙 -> 사건A와 사건B가 동시에 일어날 경우의 수
+        System.out.println("== 곱의 법칙 ==");
+
+        // 두 개의 주사위 a,b를 던겼을 때 a는 3의 배수, b는 4의 배수인 경우의 수
+        nA = 0;
+        nB = 0;
+
+        for (int item1 : dice1) {
+            if (item1 % 3 == 0) {
+                nA += 1;
+            }
+        }
+
+        for (int item2 : dice2) {
+            if (item2 % 4 == 0) {
+                nB += 1;
+            }
+        }
+
+        System.out.println("결과: " + (nA * nB));
     }
 }
+
+// 경우의 수 -> 어떤 사건에서 일어날 수 있는 경우의 가짓수
+
+
+
--- src/dataStructure/basicMath/Gong.java
+++ src/dataStructure/basicMath/Gong.java
@@ -1,4 +1,47 @@
 package dataStructure.basicMath;
 
+import java.util.ArrayList;
+
 public class Gong {
+    // 약수 구하기
+    public ArrayList<Integer> getDivisor(int num) {
+        ArrayList<Integer> result = new ArrayList();
+        // 수의 절반 까지는 나누어 떨어진다. 하지만 절반 이상이 되면 나누어 떨어지는 경우가 없다.
+        for (int i = 1; i <= (int) num / 2; i++) {
+            if (num % i == 0) {
+                result.add(i);
+            }
+        }
+        result.add(num);
+        return result;
+    }
+
+    // 최대 공약수
+    public int getGCD(int numA, int numB) {
+        int gcd = -1;
+        ArrayList<Integer> divisorA = this.getDivisor(numA);
+        ArrayList<Integer> divisorB = this.getDivisor(numB);
+        for (int itemA : divisorA) {
+            for (int itemB : divisorB) {
+                if (itemA == itemB) {
+                    if (itemA > gcd) {
+                        gcd = itemA;
+                    }
+                }
+            }
+        }
+        return gcd;
+    }
+
+    // 최소 공배수
+    public int getLCM(int numA, int numB) {
+        int lcm = -1;
+
+        int gcd = this.getGCD(numA, numB);
+        if (gcd != -1) {
+            // 최소 공배수는 두 수를 곱하고 최대 공약수로 나눠 주면 된다.
+            lcm = numA * numB / gcd;
+        }
+        return lcm;
+    }
 }
